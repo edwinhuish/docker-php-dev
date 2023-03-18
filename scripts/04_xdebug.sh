48614a9ca47c9b1c1348be1fcc198e24134792dd
edwinhuish <edwinhuish@gmail.com>
2023-03-18 16:13:43 +0800

    更新script

--- scripts/04_xdebug.sh
+++ scripts/04_xdebug.sh
@@ -8,7 +8,9 @@ echo "PHP_VERSION is: ${PHP_VERSION} ..........."
 if [[ "$PHP_VERSION" == 7* ]] ; then XDEBUG_VERSION=xdebug-3.1.5 ; fi
 echo "Begin install ${XDEBUG_VERSION} ..........."
 yes | pecl install ${XDEBUG_VERSION}
-echo "zend_extension=$(find /usr/local/lib/php/extensions/ -name xdebug.so)" > /usr/local/etc/php/conf.d/xdebug.ini
+
+XDEBUG_SO=$(find /usr/local/lib/php/extensions/ -name xdebug.so)
+echo "zend_extension=${XDEBUG_SO}" > /usr/local/etc/php/conf.d/xdebug.ini
 echo "xdebug.mode = debug" >> /usr/local/etc/php/conf.d/xdebug.ini
 echo "xdebug.start_with_request = no" >> /usr/local/etc/php/conf.d/xdebug.ini
 echo "xdebug.client_host = localhost" >> /usr/local/etc/php/conf.d/xdebug.ini
