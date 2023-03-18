7d9693e0e5f4166cf5ffdae4c18bc37fb59092f1
edwinhuish <edwinhuish@gmail.com>
2023-03-18 22:12:33 +0800

    修复composer

--- scripts/05_composer.sh
+++ scripts/05_composer.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+su www -c "composer global config --no-plugins allow-plugins.slince/composer-registry-manager true"
 su www -c "composer global require slince/composer-registry-manager"
