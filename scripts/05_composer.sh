fec187d0aa384ceba8ab1ba507b00d0ea5385220
edwinhuish <edwinhuish@gmail.com>
2023-03-18 17:39:10 +0800

    update

--- scripts/05_composer.sh
+++ scripts/05_composer.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-source /etc/profile
-su www -c "source /etc/profile"
-
 su www -c "composer global require slince/composer-registry-manager"
 
-su www -c "composer repo:use aliyun"
+su www -c "source /etc/profile && composer repo:use aliyun"
