b16620677951f14838b73b7846adbaf4e70d4dff
edwinhuish <edwinhuish@gmail.com>
2023-06-22 07:23:06 +0800

    修改PUID和GUID修改规则

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -16,8 +16,29 @@ export APACHE_RUN_USER
 : ${APACHE_RUN_GROUP:=$GROUPNAME}
 export APACHE_RUN_GROUP
 
-usermod -u $PUID $USERNAME
-groupmod -g $PGID $GROUPNAME
+
+OLD_GID=$(id -g $USERNAME)
+if [ "${PGID}" != "automatic" ] && [ "$PGID" != "$OLD_GID" ]; then
+
+  echo "修改 GID: $OLD_GID => $PGID"
+
+  if getent group $PGID > /dev/null 2>&1; then
+    echo "已存在 GID， 修改主GID"
+    usermod -g $PGID $USERNAME
+  else
+    groupmod --gid $PGID ${GROUPNAME}
+  fi
+
+fi
+
+OLD_UID=$(id -u $USERNAME)
+if [ "${PUID}" != "automatic" ] && [ "$PUID" != "$OLD_UID" ]; then
+
+  echo "修改 UID: $OLD_UID => $PUID"
+
+  usermod --uid $PUID $USERNAME
+
+fi
 
 if [ -d /docker-entrypoint.d ]; then
   for i in /docker-entrypoint.d/*.sh; do
