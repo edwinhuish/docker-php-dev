cb0af19fd8048b68489935dad4241d17488792aa
edwinhuish <edwinhuish@gmail.com>
2023-08-24 00:59:56 +0800

    增加 phpctrl

--- scripts/01_php_apache_env.sh
+++ scripts/01_php_apache_env.sh
@@ -6,9 +6,57 @@ set -e
 # APACHE
 #
 if ! echo "$VARIANT" | grep -q "apache"; then
-  exit 0
+    exit 0
 fi
 
 echo "Add ServerName to apache2.conf 。。。。"
 echo "ServerName localhost" >>/etc/apache2/apache2.conf
 ln -s /etc/apache2/mods-available/rewrite.load /etc/apache2/mods-enabled/rewrite.load
+
+cat >/usr/local/bin/phpctrl <<EOF
+#!/bin/bash
+
+EPACE='        '
+echow() {
+    FLAG=\${1}
+    shift
+    echo -e "\033[1m\${EPACE}\${FLAG}\033[0m\${@}"
+}
+
+help_message() {
+    echo -e "\033[1mOPTIONS\033[0m"
+    echow 'restart|reload'
+    exit 0
+}
+
+check_input() {
+    if [ -z "\${1}" ]; then
+        help_message
+    fi
+}
+
+restart() {
+    sudo apache2ctl restart
+    exit 0
+}
+
+check_input \${1}
+while [ ! -z "\${1}" ]; do
+    case \${1} in
+    -[hH] | -help | --help)
+        help_message
+        ;;
+    restart | reload)
+        shift
+        restart
+        ;;
+    *)
+        help_message
+        ;;
+    esac
+    shift
+done
+
+EOF
+
+chmod +x /usr/local/bin/phpctrl
--- scripts/01_php_fpm_env.sh
+++ scripts/01_php_fpm_env.sh
@@ -6,11 +6,59 @@ set -e
 # FPM
 #
 if ! echo "$VARIANT" | grep -q "fpm"; then
-  exit 0
+    exit 0
 fi
 
 if [ ! -f /usr/local/etc/php-fpm.d/www.conf ]; then
-  cp /usr/local/etc/php-fpm.d/www.conf.default /usr/local/etc/php-fpm.d/www.conf
+    cp /usr/local/etc/php-fpm.d/www.conf.default /usr/local/etc/php-fpm.d/www.conf
 fi
 
 sed -i "s/www-data/${USERNAME}/g" /usr/local/etc/php-fpm.d/www.conf
+
+cat >/usr/local/bin/phpctrl <<EOF
+#!/bin/bash
+
+EPACE='        '
+echow() {
+    FLAG=\${1}
+    shift
+    echo -e "\033[1m\${EPACE}\${FLAG}\033[0m\${@}"
+}
+
+help_message() {
+    echo -e "\033[1mOPTIONS\033[0m"
+    echow 'restart|reload'
+    exit 0
+}
+
+check_input() {
+    if [ -z "\${1}" ]; then
+        help_message
+    fi
+}
+
+restart() {
+    sudo kill -USR2 \$(pgrep php-fpm | sort -n | head -1)
+    exit 0
+}
+
+check_input \${1}
+while [ ! -z "\${1}" ]; do
+    case \${1} in
+    -[hH] | -help | --help)
+        help_message
+        ;;
+    restart | reload)
+        shift
+        restart
+        ;;
+    *)
+        help_message
+        ;;
+    esac
+    shift
+done
+
+EOF
+
+chmod +x /usr/local/bin/phpctrl
