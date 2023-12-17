e6efe636f264729c51c276743d19a65deaefecbd
edwinhuish <edwinhuish@gmail.com>
2023-12-17 21:11:18 +0800

    Add swoole

---
+++ scripts/06_swoole.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+pecl install -D 'enable-sockets="no" enable-openssl="yes" enable-http2="yes" enable-mysqlnd="yes" enable-swoole-json="no" enable-swoole-curl="yes" enable-cares="yes"' swoole
+docker-php-ext-enable swoole
