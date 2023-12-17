885fcb6308d34aff7c30c9b810dbe5d3d4c5e752
edwinhuish <edwinhuish@gmail.com>
2023-12-17 21:21:09 +0800

    Update swoole ext

--- scripts/06_swoole.sh
+++ scripts/06_swoole.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
-pecl install -D 'enable-sockets="no" enable-openssl="yes" enable-http2="yes" enable-mysqlnd="yes" enable-swoole-json="no" enable-swoole-curl="yes" enable-cares="yes"' swoole
+PHP_VERSION=$(php -v | grep "PHP" | awk '{print $2}')
+
+if [[ "$PHP_VERSION" == 7* ]]; then
+  echo "Only install swoole for php >= 8.0 ...."
+  exit 0
+fi
+
+pecl install -D 'enable-sockets="no" enable-openssl="yes" enable-http2="yes" enable-mysqlnd="no" enable-swoole-json="no" enable-swoole-curl="yes" enable-cares="yes"' swoole
 docker-php-ext-enable swoole
