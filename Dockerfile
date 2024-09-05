44a7cd456b55919094451c9c8143c927bb6e2e0f
Edwin Xu <edwinhuish@gmail.com>
2024-09-05 21:24:13 +0800

    修改 PUID GUID 默认值为 0

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,8 @@
 # [Choice] PHP version (use -bullseye variants on local arm64/Apple Silicon): 8-apache-bullseye, 8.1-apache-bullseye, 8.0-apache-bullseye, 7-apache-bullseye, 7.4-apache-bullseye, 8-apache-buster, 8.1-apache-buster, 8.0-apache-buster, 7-apache-buster, 7.4-apache-buster
 ARG VARIANT=7-apache-bullseye
 FROM edwinhuish/php-dev:${VARIANT}
+
+COPY entry.sh /entry.sh
+RUN chmod +x /entry.sh
+
+ENTRYPOINT [ "/entry.sh", "docker-php-entrypoint" ]
---
+++ entry.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+set -e
+
+: ${PUID:=0}
+: ${PGID:=0}
+
+: ${USERNAME:=www}
+
+OLD_USERNAME=$(id -nu $PUID 2>/dev/null)
+if [ -n "$OLD_USERNAME" ]; then
+  echo "PUID 已存在，使用用户名： $OLD_USERNAME"
+  USERNAME="$OLD_USERNAME"
+fi
+
+if ! egrep "^$USERNAME:" /etc/passwd >/dev/null 2>&1; then
+  echo "不存在用户，开始新增：$USERNAME"
+  useradd -s /bin/bash --uid $PUID -m $USERNAME
+fi
+
+: ${GROUPNAME:="$(id -gn $USERNAME)"}
+
+OLD_GID=$(id -g $USERNAME)
+if [ "${PGID}" != "automatic" ] && [ "$PGID" != "$OLD_GID" ]; then
+
+  echo "修改 GID: $OLD_GID => $PGID"
+
+  if getent group $PGID >/dev/null 2>&1; then
+    echo "已存在 GID， 修改主GID"
+    usermod -g $PGID $USERNAME
+  else
+    groupmod --gid $PGID ${GROUPNAME}
+  fi
+
+fi
+
+OLD_UID=$(id -u $USERNAME)
+if [ "${PUID}" != "automatic" ] && [ "$PUID" != "$OLD_UID" ]; then
+
+  echo "修改 UID: $OLD_UID => $PUID"
+
+  usermod --uid $PUID $USERNAME
+
+fi
+
+# 使用 exec 会导致后面的 script 作为主进程，导致 entry.d 的script都在它之下
+exec "$@"
